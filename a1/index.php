8013bf57725ed9da73bc1de7e4a5679858119883
Jwahn1 <javierwith@gmail.com>
2024-09-18 13:25:14 -0300

    Finished the basic look of index.php

--- a1/index.php
+++ a1/index.php
@@ -28,11 +28,62 @@
        <div class="row" style="height:200px; background-color:#dedede; border-style:solid;">
           <p>banner for picture </p>
        </div>
-       <div class="row" style="height:75%; background-color:#838685; border-style:solid;">
+       <div class="row" style="height:75%; background-color:#bab8b9; border-style:solid;">
           <div class="container" style="width:70%; background-color:#fff; border-style:solid;">
-              <div class="row mt-5" style="position:absolute; right:550px; height:150px; width:12%;background-color:#838685;">
-                  <p>User Login</p>
+          <p>inner white text paragraph</p>
+              <div class="row" style="height:150px;  border-style:none;">
+                <div class="col-8" style="background-color:#fff; border-style:none;overflow-wrap: break-word; ">
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                 </p>                                  
+                  
+                  <a href="info.php">more info here!</a>
+                </div>
+                
+                <div class="col-3" style="background-color:#bab8b9;">
+                <form action="" get="get">
+                    <label for="i-keywords">User Login</label>
+                    <input type="text" name="username" id="username" required style="width:150px; borderstyle:solid; border-radius: 5px;">
+                    <input type="text" name="password" id="password" required style="width:150px; borderstyle:solid; border-radius: 5px;">
+                    <input type="submit" value="login" name="submit" i="i-submit">
+                  </form>
+                </div>
+              
+            <div class="row mt-3">
+              <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+              </p>
+            </div>
+            <div class="row ml-4" style="background-color:#000202; width:90%;">
+               <h4 style="color :#fff">
+                  File read task 1 (give appropriate heading)
+               </h4>
+            </div>
+            <div class="row">
+              <div class="col-6 ml-4 mt-2" style="overflow-wrap: break-word; border-style:none;background-color:#c7dbf7"> 
+                <p>The text from the File read task 1 on
+                Brightspace must be displayed here.</p>
               </div>
+              <div class="col-5 ml-2 mt-2" style="overflow-wrap: break-word; border-style:none;background-color:#c7dbf7"> 
+                <p>img here</p>
+              </div>
+            </div>
+            <div class="row ml-4 mt-4" style="background-color:#000202; width:90%;">
+               <h4 style="color :#fff">
+                  File read task 2 (give appropriate heading)
+               </h4>
+            </div>
+            <div class="row">
+              <div class="col-6 ml-4 mt-2" style="overflow-wrap: break-word; border-style:none;background-color:#c7dbf7"> 
+                <p>The text from the File read task 2 on
+                Brightspace must be displayed here.</p>
+              </div>
+              <div class="col-5 ml-2 mt-2" style="overflow-wrap: break-word; border-style:none;background-color:#c7dbf7"> 
+                <p>img here</p>
+              </div>
+            </div>
           </div>
        </div>
     </div>
