7dfd0ef9556f90411fe2aeaaaf7834c09bfbad41
Jwahn1 <javierwith@gmail.com>
2024-09-18 00:03:01 -0300

    layout for index.php is almost ready, need to add query, paragraph and two tables

--- a1/index.php
+++ a1/index.php
@@ -7,25 +7,34 @@
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link href="css/styles.css" rel="stylesheet">
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="css/boostrap-grid.css" rel="stylesheet">
     <title>Hello, world!</title>
   </head>
   <body>
-    <h1>Hello, world!</h1>
+
     <?php
        
 
     ?>
-
-    <!-- Optional JavaScript; choose one of the two! -->
-
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-
-    <!-- Option 2: Separate Popper and Bootstrap JS -->
-    <!--
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
-    -->
+    <div class="container mt-4 " style="background-color:#fff; height:900px;">
+       <div class="row" style="height:50px; background-color:#78777b">
+        <div class="col-1">logo</div>
+        <div class="col-8"></div>
+        <div class="col-1">nav1</div>
+        <div class="col-1">nav2</div>
+        <div class="col-1">email me</div>
+       </div>
+       <div class="row" style="height:200px; background-color:#dedede; border-style:solid;">
+          <p>banner for picture </p>
+       </div>
+       <div class="row" style="height:75%; background-color:#838685; border-style:solid;">
+          <div class="container" style="width:70%; background-color:#fff; border-style:solid;">
+              <div class="row mt-5" style="position:absolute; right:550px; height:150px; width:12%;background-color:#838685;">
+                  <p>User Login</p>
+              </div>
+          </div>
+       </div>
+    </div>
   </body>
 </html>
