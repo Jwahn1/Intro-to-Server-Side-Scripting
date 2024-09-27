ed2997510f3e8ab68107e4cfe047dddd6ce26a01
Jwahn1 <javierwith@gmail.com>
2024-09-27 16:23:13 -0300

    Forgot to add navigation and change banner text, now it should be all done

--- a1/includes/header.php
+++ a1/includes/header.php
@@ -1,10 +1,10 @@
 <div class="row" style="height:50px; background-color:#78777b">
-        <div class="col-1" style="font-size:20px;">logo</div>
+        <div class="col-1" style="font-size:20px;"></div>
         <div class="col-8" ></div>
-        <div class="col-1 "style="font-size:20px;">nav1</div>
-        <div class="col-1"style="font-size:20px;">nav2</div>
+        <div class="col-1 "style="font-size:20px;"> <a href="http://localhost/2170/a1/index.php">Nav1</a> </div>
+        <div class="col-1 "style="font-size:20px;"> <a href="http://localhost/2170/a1/info.php">Nav2</a> </div>
         <div class="col-1"style="font-size:20px;"><a href="mailto:jv513910@dal.ca?subject=mail from A1">Email</a></div>
        </div>
-       <div class="row" style="height:200px; background-color:#dedede; border-style:solid;">
-          <p>banner for picture </p>
+       <div class="row" style="height:200px; background-color:#dedede; border-style:solid; text-align:center;">
+          <h1>Welcome to Assignment 1 webisite</h1>
        </div>
