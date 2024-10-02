e4db5570bf6d10e0f74515bf95bc1bbd73688d81
Jwahn1 <javierwith@gmail.com>
2024-10-02 14:12:08 -0300

    modfied index to have login and signup links and to display the corrently logged in user's email through a set cookie. The cookie is set upon a successful login in login-processing.php

--- Lab 3/includes/login-processing.php
+++ Lab 3/includes/login-processing.php
@@ -21,7 +21,10 @@ if(!$emailcorrect){
     echo "Error 404, Email not found";
 }elseif(!$passwordcorrect){
     echo "The inputted password is wrong";
-}elseif{
-    echo "successful login";
+}else{
+    //if both email and password are correct, log the email into the cookie array
+    setcookie("user", $email, time() + (86400 * 30), "/");
 }
+
+echo "<p>Login Success</p><br><a href='http://localhost/2170/Lab%203/index.php'>Return to main page</a> ";
 ?>
