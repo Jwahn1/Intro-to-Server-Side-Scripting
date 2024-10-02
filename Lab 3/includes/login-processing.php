0a7fa50b06b10de650a15589fdf88ed634959262
Jwahn1 <javierwith@gmail.com>
2024-10-02 13:09:18 -0300

    added login functionality

---
+++ Lab 3/includes/login-processing.php
@@ -0,0 +1,25 @@
+<?php
+$email = $_POST['email'];
+$password = $_POST['password'];
+$database = fopen("../db/user-data.csv","r");
+$emailcorrect = false;
+$passwordcorrect = false;
+
+$storeEmail = "";
+
+//first we want to check if this used email is already in the database
+while ($storeEmail = fgetcsv($database)) {
+    if($storeEmail[0] == $email ) {
+        $emailcorrect = true;
+    }
+    if($storeEmail[1] == $password){
+        $passwordcorrect = true;
+    }
+}
+
+if(!$emailcorrect){    
+    echo "Error 404, Email not found";
+}elseif(!$passwordcorrect){
+    echo "The inputted password is wrong";
+}
+?>
--- Lab 3/login.php
+++ Lab 3/login.php
@@ -7,10 +7,14 @@
     <title>signup</title>
 </head>
 <body>
-<form action="includes/form-processing.php" method="POST">
-    <input type="email" name="email" required>
-    <input type="password" name="password" required>
-    <button type="submit" id="i-submit" name="login-submit">Submit</button>
+<form action="includes/signup-processing.php" method="POST">
+    <label for="email">Email:</label>
+    <input type="email" id="email" name="email" required><br>
+
+    <label for="password">Passowrd:</label>
+    <input type="password" id="password" name="password" required><br>
+
+    <button type="submit" id="i-submit" name="message-submit">Submit</button>
 </form>
 </body>
 </html>
