b7d13f1110da89d1e725ee4bd967b6958e7ed457
Jwahn1 <javierwith@gmail.com>
2024-10-16 11:16:37 -0300

    register form ready to implement with scripting

--- a2/includes/footer.php
+++ a2/includes/footer.php
@@ -5,7 +5,7 @@
     <div class="row" style="height:80px;  background-color:#2222; ">
         <div class="col-1" style="font-size:20px; "></div>
         <div class="col-1" style="font-size:20px; "></div>
-        <div class="col-1"style="font-size:15px;text-align: left ;"><a href="">Home</a><br><a href="">Preferences</a><br><a href="">logout</a></div>
+        <div class="col-1"style="font-size:15px;text-align: left ;"><a href="http://localhost/2170/a2/index.php">Home</a><br><a href="">Preferences</a><br><a href="">logout</a></div>
         <div class="col-1"style="font-size:15px;text-align: left ;"><a href="">About myJournal</a><br><a href="">Terms of Use</a><br><a href="">Privacy Policy</a></div>
         <div class="col-4" style="font-size:20px; "><p>Copyright 1999-2024 by Javier Wahn. All Rights Reserved.</p></div>
     </div>
--- a2/register.php
+++ a2/register.php
@@ -3,7 +3,26 @@ include "includes/header.php";
 ?>
 
 <main>
-
+<div class=" container" style="background-color:#fff; border-style:solid; height: 800px;  ">
+    <div class="col-3" style="background-color:#bab8b9; height:300px; width:380px; text-align:left">
+    <h3>REGISTER</h3>
+        <form action="index.php" method="post">
+          <label for="i-keywords">Username</label>
+          <input type="text" name="username" id="username" required style="margin-left:50px;width:150px; borderstyle:solid; border-radius: 5px;"><br>
+          <label for="i-keywords">Password</label>
+          <input type="text" name="password" id="password" required style="margin-left:54px;px;width:150px; borderstyle:solid; border-radius: 5px;"><br>
+          <label for="i-keywords">Verify Password</label>
+          <input type="text" name="verify-password" id="verify-password" required style="margin-left:12px;width:150px; borderstyle:solid; border-radius: 5px;"><br>
+          <label for="i-keywords">Full name</label>
+          <input type="text" name="full-name" id="full-name" required style="margin-left:54px;width:150px; borderstyle:solid; border-radius: 5px;"><br>
+          <label for="i-keywords">Security Question</label>
+          <input type="text" name="security-question" id="security-question" required style="width:150px; borderstyle:solid; border-radius: 5px;"><br>
+          <label for="i-keywords">Security Answer</label>
+          <input type="text" name="security-answer" id="security-answer" required style="margin-left:12px;width:150px; borderstyle:solid; border-radius: 5px;"><br>
+          <input type="submit" value="register" name="submit" i="i-submit">
+          </form>
+      </div>
+  </div>
 </main>
 
 <?php 
