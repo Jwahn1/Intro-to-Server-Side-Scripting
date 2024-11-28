0b3b2bd72d9a22ef8d9bde31a77e8743be5d2acf
Jwahn1 <javierwith@gmail.com>
2024-11-28 16:25:12 -0400

    Started Login implementation

---
+++ a4/Login_Page.php
@@ -0,0 +1,19 @@
+<?php
+    // Index functionality
+    include "includes/config.php";
+    include "templates/header.php";
+
+ ?>
+<div class=" container" style="background-color:#fff; border-style:solid; height: 800px;  ">
+    <div class="col-3 mt-2" style="background-color:#bab8b9; height:300px; width:380px; text-align:left">
+    <h3>LOGIN</h3>
+        <form action="includes/login.php" method="post">
+          <label for="i-keywords">Username</label>
+          <input type="text" name="username" id="username" required style="margin-left:50px;width:150px; borderstyle:solid; border-radius: 5px;"><br>
+          <label for="i-keywords">Password</label>
+          <input type="text" name="password" id="password" required style="margin-left:54px;px;width:150px; borderstyle:solid; border-radius: 5px;"><br>
+
+          <input type="submit" value="register" name="submit" i="i-submit">
+          </form>
+      </div>
+  </div>
