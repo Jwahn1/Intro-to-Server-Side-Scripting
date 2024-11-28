a2879de9c83b89196bf5668be67a5fcd1b732a47
Jwahn1 <javierwith@gmail.com>
2024-11-28 16:25:31 -0400

    Preparing to add login header functions soon

--- a4/templates/header.php
+++ a4/templates/header.php
@@ -1,7 +1,4 @@
-<?php
-// Header functionality
-?>
-<!doctype html>
+
 <html lang="en">
   <head>
     <!-- Required meta tags -->
@@ -17,8 +14,12 @@
   <div class="row" style="height:50px; background-color:#78777b">
     <div class="col-3" style="font-size:20px;">DALHOUSIE FORUM</div>
     <div class="col-8" ></div>
-    <div class="col-1 "style="font-size:20px;"> <a href="http://localhost/2170/a1/index.php">Nav1</a> </div>
-    <div class="col-1 "style="font-size:20px;"> <a href="http://localhost/2170/a1/info.php">Nav2</a> </div>
+    
+    <?php
+    //we should add an if statement that checks if the user is logged and in and then display the rest of the nav
+    ?>
+    <!doctype html>
+    
 
    </div>
  
